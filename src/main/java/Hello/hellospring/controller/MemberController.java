fa2190d7a7624c4eb6418f2fa9b0e908917b1d31
Kimhyundo <kk090303@naver.com>
2021-01-06 21:38:05 +0900

    Register spring bin directly with Java code

--- src/main/java/Hello/hellospring/controller/MemberController.java
+++ src/main/java/Hello/hellospring/controller/MemberController.java
@@ -5,14 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 
-
 @Controller
 public class MemberController {
 
     private final MemberService memberService;
 
+    //생성자 주입
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+
 }
--- src/main/java/Hello/hellospring/service/MemberService.java
+++ src/main/java/Hello/hellospring/service/MemberService.java
@@ -8,22 +8,10 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
 
-    //회원 서비스 제공을 위한 repository 생성
-    //DI(의존성 주입) 을 위해 코드상에서 객체를 직접적으로 만드는게 아니라
-    //객체의 밖에서 객체를 넣어주는 방식이다.
-    //생성자를 통해 memberRepository를 생성해주면 의존성을 주입 받을 수 있다.
-    //memaberRepository에 다양한 객체를 받아서 의존성을 주입 받을 수 있다.
 
-    //before
-    /* 이렇게 new를 통해 직접 객체를 생성하면 MemberSeirvice 는 MemoryMemberRepository에 의존이 되어버린다.
-    private final MemberRepository memberRepository = new
-            MemoryMemberRepository();
-     */
-
-    //after
+    //DI
     private final MemberRepository memberRepository;
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
